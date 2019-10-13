dcff0739b4dffdfec1feb1b267016f19a1f8ddd5
zoomout <bzastavnyi@gmail.com>
2019-10-13 03:02:42 +0200

    init commit

---
+++ src/main/java/com/bogdan/springpersistentweb/SpringPersistentWebApplication.java
@@ -0,0 +1,13 @@
+package com.bogdan.springpersistentweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersistentWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPersistentWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogdan/springpersistentweb/SpringPersistentWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bogdan.springpersistentweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPersistentWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
