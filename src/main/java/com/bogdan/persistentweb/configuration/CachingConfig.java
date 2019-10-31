dd13aaaf7927853993eedb491d84bb951cd92eda
zoomout <bzastavnyi@gmail.com>
2019-10-31 20:52:39 +0100

    using ehcache

--- src/main/java/com/bogdan/persistentweb/configuration/CachingConfig.java
+++ src/main/java/com/bogdan/persistentweb/configuration/CachingConfig.java
@@ -1,10 +1,7 @@
 package com.bogdan.persistentweb.configuration;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,10 +14,5 @@ public class CachingConfig {
 
   public static final String CUSTOMER_CACHE = "com.bogdan.persistentweb.domain.Customer";
   public static final String PRODUCT_CACHE = "com.bogdan.persistentweb.domain.Product";
-
-  @Bean
-  public CacheManager cacheManager() {
-    return new ConcurrentMapCacheManager(CUSTOMER_CACHE, PRODUCT_CACHE);
-  }
 
 }
